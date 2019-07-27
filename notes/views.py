6843578680a88cc25c7bdea87b61dcae0fb96829
encqre <47271277+encqre@users.noreply.github.com>
2019-07-27 05:56:51 +0300

    updated views with logging

--- notes/views.py
+++ notes/views.py
@@ -4,11 +4,11 @@ from django.urls import reverse
 from django.core.serializers.json import DjangoJSONEncoder
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from .models import Entry, Update, Category, Profile
+from .models import Entry, Update, Category, Profile, Log
 
 from datetime import datetime
+from calendar import month_name
 import json
-import requests
 
 ## Rendered pages
 
@@ -66,8 +66,35 @@ def notes(request):
 
 def history(request):
     if request.user.is_authenticated:
-        all_updates = Update.objects.all().order_by("-date")
-        return render(request, "notes/history.html", {"updates":all_updates})
+        
+        got_year = request.GET.get("year")
+		current_year = datetime.now().year
+		# current_year = 2024
+		display_year_list = [current_year]
+		if current_year > 2019:
+			for year in reversed(range(2019,current_year)):
+				display_year_list.append(year)
+		got_month = request.GET.get("month")
+
+		if got_year != None:
+			got_year = int(str(got_year))
+
+		if got_month != None:
+			got_month = int(str(got_month))
+		
+		if got_month in range(1,13):
+			month = got_month
+		else:
+			month = datetime.now().month
+
+		if got_year in range (2019,2100):
+			year = got_year
+		else:
+			year = current_year
+            
+        #all_logs = Log.objects.all().order_by("-date")
+        month_logs = Log.objects.filter(date__month=month, date__year=year).order_by("-date")
+        return render(request, "notes/history.html", {"logs":month_logs, "year":year, "month": month, "month_name":month_name[month], "current_year":current_year, "years":display_year_list})
     else:
         return redirect('/?message=no_auth')
 
@@ -75,8 +102,8 @@ def my_updates(request):
     if request.user.is_authenticated:
         user = User.objects.filter(username=request.user.username)[0]
         profile = Profile.objects.filter(user=user)[0]
-        my_updates = Update.objects.filter(updater=profile).order_by("-date")
-        return render(request, "notes/my_updates.html", {"updates":my_updates, "profile":profile})
+        my_logs = Log.objects.filter(updater=profile).order_by("-date")
+        return render(request, "notes/my_updates.html", {"logs":my_logs, "profile":profile})
     else:
         return redirect('/?message=no_auth')
 
@@ -88,7 +115,14 @@ def remove(request):
         update = Update.objects.filter(uuid=uuid)[0]
         entry = update.entry
         entry.active = False
-        entry.save(0)
+        entry.save()
+        
+        user = User.objects.filter(username=request.user.username)[0]
+		profile = Profile.objects.filter(user=user)[0]
+		date = datetime.now()
+
+		log_update(profile, update, "DEL", date=date)
+        
         response_data = "Entry " + str(uuid) + " removed"
         return HttpResponse(
             json.dumps(response_data),
@@ -104,6 +138,7 @@ def edit(request):
         content = request.POST.get('content')
         user = User.objects.filter(username=request.user.username)[0]
         profile = Profile.objects.filter(user=user)[0]
+        date = datetime.now()
 
         old_update = Update.objects.filter(uuid=uuid)[0]
         entry = old_update.entry
@@ -111,6 +146,9 @@ def edit(request):
         new_update.save()
         entry.last_updated = new_update.date
         entry.save()
+        
+        log_update(profile, new_update, "EDIT", date=date)
+        
         response_data = {"status":"updated", "title":title, "content":content, "new_uuid":new_update.uuid, "updater":profile.short_name, "date":new_update.date}
         return HttpResponse(
             json.dumps(response_data, cls=DjangoJSONEncoder),
@@ -127,15 +165,18 @@ def new(request):
         content = request.POST.get('content')
         user = User.objects.filter(username=request.user.username)[0]
         profile = Profile.objects.filter(user=user)[0]
+        date = datetime.now()
 
         if (category == "ONE"):
             categoryObject = Category.objects.filter(name="One")[0]
         elif (category == "TWO"):
             categoryObject = Category.objects.filter(name="Two")[0]
-        entry = Entry(original_title=title, active=True, category=categoryObject, create_date=datetime.now(), last_updated=datetime.now(), created_by=profile)
+        entry = Entry(original_title=title, active=True, category=categoryObject, create_date=date, last_updated=date, created_by=profile)
         entry.save()
-        update = Update(uuid=uuid, title=title, content=content, entry=entry, date=datetime.now(), updater=profile)
+        update = Update(uuid=uuid, title=title, content=content, entry=entry, date=date, updater=profile)
         update.save()
+        
+        log_update(profile, update, "ADD", date=date)
 
         response_data = {"status": "created", "title":title, "content":content, "uuid":uuid, "updater":profile.short_name, "date":update.date}
         return HttpResponse(
@@ -156,3 +197,17 @@ def get_content(request):
             )
     else:
         return HttpResponse("What are you doing here?")
+    
+   
+#MISC 
+
+def log_update(profile, update, type, date=datetime.now()):
+	dateString = str(date).split(".")[0]
+	if type == "EDIT":
+		message = str(dateString) + " - " + str(profile.full_name) + " updated existing entry"
+	elif type == "ADD":
+		message = str(dateString) + " - " + str(profile.full_name) + " added new entry"
+	elif type == "DEL":
+		message = str(dateString) + " - " + str(profile.full_name) + " deleted entry"
+	new_log = Log(log=message , updater=profile, date=date, update=update, type=type)
+	new_log.save()
